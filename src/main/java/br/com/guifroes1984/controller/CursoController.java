d1e89ab07c0723c972c980af7d83b6669cb42268
guifroes1984 <guilhermefroes26@gmail.com>
2023-08-02 14:41:18 -0300

    Configuração do banco de dados, e a ligação do backend com frontend

--- src/main/java/br/com/guifroes1984/controller/CursoController.java
+++ src/main/java/br/com/guifroes1984/controller/CursoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.guifroes1984.model.Curso;
@@ -18,7 +19,7 @@ public class CursoController {
 	private final CursoRepository cursoRepository;
 
 	@GetMapping
-	public List<Curso> lista() {
+	public @ResponseBody List<Curso> lista() {
 		return cursoRepository.findAll();
 	}
 }
